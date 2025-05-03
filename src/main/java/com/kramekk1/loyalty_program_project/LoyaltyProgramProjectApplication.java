df3eb12266106080a50552b195add72559c84244
Jakub <kramekk1@gmail.com>
2025-05-03 12:08:40 +0200

    initial commit

---
+++ src/main/java/com/kramekk1/loyalty_program_project/LoyaltyProgramProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kramekk1.loyalty_program_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyProgramProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyProgramProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kramekk1/loyalty_program_project/LoyaltyProgramProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kramekk1.loyalty_program_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyProgramProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
